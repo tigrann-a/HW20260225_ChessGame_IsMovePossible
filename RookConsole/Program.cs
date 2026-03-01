b45e3873a6218cfcd3ba9d4c5523ae8b2254408c
Tigran <taginian1991@hotmail.com>
2026-03-01 23:48:15 +0400

    Adding GetCoords method

--- RookConsole/Program.cs
+++ RookConsole/Program.cs
@@ -1,7 +1,20 @@
 ﻿using static System.Console;
+using static System.Net.WebRequestMethods;
+using LibraryProject;
+using LibraryProject.Pieces;
 
 WriteLine("Please enter coordinates of White Rook: ");
 string whiteRookPosTxt = ReadLine();
 
 WriteLine("Please enter coordinates of Black Rook: ");
 string  blackRookPosTxt = ReadLine();
+
+void GetCoords(string pos, out byte x, out byte y)
+{
+    bool isNum = byte.TryParse(pos[1].ToString(), out x);
+    x -= 1;
+
+    y = (byte)Enum.Parse<Files>(pos[0].ToString());
+
+    WriteLine($"X: {x}, Y: {y}");
+}
